8cd54e10301187706cbe67f1c79a0ca85a204d10
jeosafaferreira <51677471+jeosafaferreira@users.noreply.github.com>
2019-07-12 19:59:17 -0300

    Update q2.c

--- Lista2/q2.c
+++ Lista2/q2.c
@@ -7,10 +7,10 @@
 O programa deve tamb ́em imprimir tais matrizes, calcular a determinante de cada uma delas e calcular o resultado da multiplica ̧c ̃ao das duas.
 */
 
-int main(){
+int main(void){
   srand(time(NULL));
-  int mat1[3][3];
-  int mat2[3][3];
+  int mat1[4][4];
+  int mat2[4][4];
 
   //Populando matrizes
   for(int i=0;i<4;i++){
@@ -22,11 +22,20 @@ int main(){
 
   printf("Matriz 1:\n");
   for(int i=0;i<4;i++){
+    printf("[");
     for(int j=0;j<4;j++){
-      printf("%d ",mat1[i][j]);
+      printf("%4.0d ",mat1[i][j]);
     }
-    printf("\n");
+    printf("]\n");
   }
-  
-  
+
+  printf("\n Matriz 2:\n");
+  for(int i=0;i<4;i++){
+    printf("[");
+    for(int j=0;j<4;j++){
+      printf("%4.0d ",mat2[i][j]);
+    }
+    printf("]\n");
+  }
+  //aplicar laplace e sarrus
 }
