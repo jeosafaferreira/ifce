fe7a4a9a39fc9bd9d014ba072f37d05e13e1aa52
jeosafaferreira <51677471+jeosafaferreira@users.noreply.github.com>
2019-07-12 15:37:43 -0300

    Update q1.c

--- Lista2/q1.c
+++ Lista2/q1.c
@@ -13,14 +13,20 @@ Logo, implemente tamb ́em uma fun ̧c ̃ao que ordene o vetor e chame-o antes d
 */
 int media();
 int moda();
+int mediana();
 
 int main(void) {
-  //Criando vetor
   srand(time(NULL));
+
+  printf("Números gerados:\n");
+
   int vetor[100];
+  //Criando vetor
   for(int i=0;i<101;i++){
     vetor[i] = 1 + rand() % 499;
+    printf("%d,", vetor[i]);
   }
+  printf("\n-------------------------------------\n");
   printf("Média: %d\n", media(vetor));
   printf("Moda: %d\n", moda(vetor));
 
@@ -36,15 +42,27 @@ int media(int vetor[]){
 
 int moda(int vetor[]){
   
-  int map[500] = {0}, moda = 0;
-  for(int i=0;i<101;i++){
+  int map[500];
+  //limpando o lixo (zerando vetor)
+  for(int i=0;i<501;i++)
+    map[i] = 0;
+
+  //contabilizando aparições
+  for(int i=0;i<101;i++)
     map[vetor[i]]++;
-  }
 
   //buscando o mais repetido
+  int moda = 0;
+  int qtd = 0;
   for(int i=0;i<501;i++){
-    if(map[i]> moda)
-      moda = map[i];
+    if(map[i]> qtd){
+      qtd = map[i];
+      moda = i;
+    }
   }
   return moda;
+}
+
+int mediana(int vetor[]){
+  
 }
